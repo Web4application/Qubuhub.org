4987d03d18383c757c9117ee21f8d7f3fa64dbc7
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-05-21 00:02:03 +0100

    Update and rename brew.sh to bin/bash.sh

---
+++ bin/bash.sh
@@ -0,0 +1,6 @@
+$ echo git add origin  "qubuhub.org
+$ echo git  remote add origin https://github.com/qubuhub/repositories
+$ echo git remote add origin https://github.com/gh-io/sglang.git
+$ echo git add origin https://github.com/Web4application/web4application.github.io.git
+git branch -M main
+git push -u origin main
