515ba69d7646ae09fdc303f455e7ceffc90e8c95
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-05-21 00:12:13 +0100

    Update and rename env.sgl to .pak.sh

---
+++ bin/bash/.pak.sh
@@ -0,0 +1,6 @@
+$ echo remote git add origin  "qubuhub.org"
+git  remote add origin https://github.com/qubuhub/repositories
+git remote add origin https://github.com/gh-io/sglang.git
+git add origin https://github.com/Web4application/web4application.github.io.git
+git branch -M main
+git push -u origin main
